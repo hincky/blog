448ffa1379c6f8efec002cb8f92ba45f5a372933
hincky <hincky@yeah.net>
2022-09-23 23:39:20 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Hincky的博客',
+  description: 'All by Hincky',
+  base: '/',
+  dest: './ROOT',  // 设置输出目录
+      head: [],
+      plugins: [],
+      themeConfig: {
+          // 添加导航栏
+          nav: [
+  		{ text: '主页', link: '/' },
+  		{ text: '指南', link: '/guide/' },
+  		{ text: 'java相关',
+  	  	    items: [
+  			{ text: 'java基础', link: '/java/java-base/' },
+  			{ text: 'java与opc通信', link: '/java/java-opc/' }
+  		    ]
+  	        }
+          ],
+      sidebar: [],
+      sidebarDepth: 2,
+      lastUpdated: 'Last Updated'
+  }
+}
+
