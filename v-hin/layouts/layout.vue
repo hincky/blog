2771c080309fea3d4a121bcfed9ca474c5d874d2
hincky <yyjtech@outlook.com>
2022-10-28 16:51:39 +0800

    update index.js

--- v-hin/layouts/layout.vue
+++ v-hin/layouts/layout.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="theme-container">
-      <Content/>
-    </div>
-  </template>
+  <div class="theme-container">
+    <Content/>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
