2fec5d3fb21022400a6cd0a8013adecd897ec236
hincky <yyjtech@outlook.com>
2022-10-30 09:47:19 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,91 @@
 module.exports = {
   title: 'hincky的博客',
   description: 'hincky的个人技术博客,用于记录后端技术相关的文档、笔记。不定期更新,随缘更新,有缘关注。',
-  // 移动端优化
   head: [
     ['link', { rel: 'icon', href: '/assets/img/h-icon.jpg' }],
+    // 下面这一行是对移动端优化
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  // theme: 'reco', //配置主题为reco
+  base: '/docs/', //github发布的访问路径
+  theme: 'reco', //配置主题为reco
+  // theme: '@vuepress/blog', //主题配置为博客类型
   themeConfig: {
     logo: '/assets/img/earth-logo-removebg.png', //导航栏logo
+    authorAvatar: '/avatar.png', //首页头像，搭配reco主题
     displayAllHeaders: true,  //侧边栏显示所有页面的标题链接
     lastUpdated: 'Last Updated',
     docsDir: 'docs', // 编辑的文件夹
     editLinks: true, // 启用编辑
     editLinkText: '编辑',
-    // base: '/',
-    // dest: '.vuepress/dist',  // 设置输出目录
-    // repo:  'https://github.com/hincky/vuepress', //导航栏右侧生成GitHub链接
+    record: '粤ICP备2022120427号', //ICP 备案文案;配合reco主题使用
+    recordLink: 'https://beian.miit.gov.cn/', //ICP 备案指向链接;配合reco主题使用
+    // cyberSecurityRecord: '公安部备案文案', //公安部备案文案;配合reco主题使用
+    // cyberSecurityLink: '公安部备案指向链接', //公安部备案指向链接;配合reco主题使用
+    startYear: '2022', // 项目开始时间，只填写年份;配合reco主题使用
+    author: 'hincky', //作者姓名，配合reco主题使用
+    authorAvatar: '/assets/img/avatar.png',//作者头像，配合reco主题使用
     sidebarDepth: 2, //侧边栏显示深度，默认1，最大2（显示到h3标题）
     subSidebar: 'auto', //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
-    sidebar: 'auto',
-    nav: [],
+    // sidebar: 'auto',
+    sidebar: {
+      // '/guide/vue/': [
+      //   'test01', 'test02', 'test03'
+      // ],
+      '/oauth2/': [
+        // 'test01', 'test02', 'test03'
+        {
+          title: '原理',
+          collapsable: true,
+          children: ['oauth2','1-template-grammar','2-data-mount','3-el-data','4-mvvm']
+        },
+        {
+          title: '应用',
+          collapsable: true,
+          children: ['1-non-single-file','2-single-file']
+        }
+      ],          
+      // '/': [''] //不能放在数组第一个，否则会导致右侧栏无法使用
+    },
+    nav: [
+      { text: '主页', link: '/', icon: 'reco-home' },
+      // { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }, //时间线
+      // { text: '分类', link: '/categories/' },
+      // { text: '归档', link: '/archives/' },
+      // { text: '友链', link: '/links/' },
+      // { text: '关于', link: '/about/' },
+      // { text: '域名', link: '/domains/' },
+      // {
+      //   text: '语言',
+      //   items: [
+      //     { text: '中文', link: '/cn/' },
+      //     { text: '英语', link: '/en/' }
+      //   ]
+      // }
+      {
+        text: 'oauth2',link: '/oauth2/'
+        // items: [
+        //   { text: '原理', link: '/oauth2/' },
+        //   { text: '应用', link: '/oauth2/action/' }
+        // ]
+      }
+    ],
+    blogConfig: { //博客设置
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      }
+    }
   },
-
-  // plugins: [],
+  plugins: [
+    // '@vuepress/pwa',
+    // {
+    //   serviceWorker: true,
+    //   updatePopup: true
+    // }
+  ],
 }
 
---
+++ docs/oauth2/1-template-grammar.md
@@ -0,0 +1,93 @@
+# 模板语法
+
+最先接触的模板
+
+```html
+<div id="demo">
+    <h1>Hello {{name.toUpperCase()}}  {{address}}</h1>
+</div>
+```
+## 插值语法
+
+功能单一，将指定的值，放在指定的位置
+
+插值语法
+```html
+{{表达式}}
+```
+> 使用位置
+
+在标签体内部，也就是：<> `在这里写` </>
+
+> 例子
+
+```html
+<div id="app">
+  {{ message }}
+</div>
+<script>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+  },
+})
+</script>
+```
+
+## 指令语法
+
+`v-指令:html标签属性="表达式"`
+
+最先接触的指令语法
+
+`v-bind:html标签属性="表达式"`  
+
+简写->  `:html标签属性="表达式"` 
+
+> 使用位置
+
+在标签属性内部，也就是：<  `在这里写`></>
+
+> 例子
+
+```html
+<div id="app">
+    <h1 v-bind:herf="url">这是一个链接</h1>
+</div>
+
+<script>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+  },
+})
+</script>
+```
+
+这里的表达式就是读取Vue实例data中具体的属性。比如表达式为message，读取Vue实例data中的message属性
+
+可以用层级去对应表达式里面的值
+
+```html
+<div id="app">
+    <h1 v-bind:herf="website.url">这是一个链接</h1>
+</div>
+
+<script>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    website: {
+        url: 'www.hincky.com',
+    },
+  }
+})
+</script>
+```
+
+
+
+
