15dd4feb28fe11e60baf373a81aa3f4652e0f66f
hincky <yyjtech@outlook.com>
2022-11-01 15:41:00 +0800

    update vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -60,66 +60,89 @@ module.exports = {
         text: '前端',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
-          // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-          {
+          { // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
             text: '前端文章',
-            items: [
-              { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-            ],
+            // items: [
+            //   { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+            // ],
           },
           {
             text: '学习笔记',
             items: [
-              { text: '《JavaScript教程》', link: '/note/javascript/' },
-              { text: '《JavaScript高级程序设计》', link: '/note/js/' },
+              // { text: '《JavaScript教程》', link: '/note/javascript/' },
+              // { text: '《JavaScript高级程序设计》', link: '/note/js/' },
               { text: '《ES6 教程》', link: '/note/es6/' },
               { text: '《Vue》', link: '/note/vue/' },
-              { text: '《React》', link: '/note/react/' },
-              {
-                text: '《TypeScript 从零实现 axios》',
-                link: '/note/typescript-axios/',
-              },
-              {
-                text: '《Git》',
-                link: '/note/git/',
-              },
-              {
-                text: 'TypeScript',
-                link: '/pages/51afd6/',
-              },
-              {
-                text: 'JS设计模式总结',
-                link: '/pages/4643cd/',
-              },
+              { text: '《React》', link: '/note/react/' }
             ],
           },
         ],
       },
       {
         text: '后端',
+        link: '/backend/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
-          { text: 'spring', link: '/backend/spring/' }
-        ]
+          { // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+            text: '后端文章',
+            // items: [
+            //   { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+            // ],
+          },
+          {
+            text: '学习笔记',
+            items: [
+              // { text: '《JavaScript教程》', link: '/note/javascript/' },
+              // { text: '《JavaScript高级程序设计》', link: '/note/js/' },
+              { text: '《ES6 教程》', link: '/note/es6/' },
+              { text: '《Vue》', link: '/note/vue/' },
+              { text: '《React》', link: '/note/react/' }
+            ],
+          },
+        ],
       },
       {
-        text: '数据库',
+        text: '运维',
+        link: '/operation/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
-          { text: 'sql', link: '/database/sql/' }
-        ]
+          { // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+            text: '运维文章',
+            // items: [
+            //   { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+            // ],
+          },
+          {
+            text: '学习笔记',
+            items: [
+              // { text: '《JavaScript教程》', link: '/note/javascript/' },
+              // { text: '《JavaScript高级程序设计》', link: '/note/js/' },
+              { text: '《ES6 教程》', link: '/note/es6/' },
+              { text: '《Vue》', link: '/note/vue/' },
+              { text: '《React》', link: '/note/react/' }
+            ],
+          },
+        ],
       },
       {
-        text: 'oauth2',
+        text: '其他技术',
+        link: '/operation/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
-          { text: '原理', link: '/oauth2/' },
-          { text: '应用', link: '/oauth2/action/' },
-        ]
-      },
-      { text: '小工具合集',
-        items: [
-        { text: 'docker命令', link: '/tools/docker-command/' },
-        { text: 'kubectl命令', link: '/tools/k8s-command/' },
-        { text: 'git命令', link: '/tools/git-command/git' },
-        ]
+          { // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+            text: '计算机科学CS',
+            // items: [
+            //   { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+            // ],
+          },
+          {
+            text: '学习笔记',
+            items: [
+              // { text: '《JavaScript教程》', link: '/note/javascript/' },
+              // { text: '《JavaScript高级程序设计》', link: '/note/js/' },
+              { text: '《ES6 教程》', link: '/note/es6/' },
+              { text: '《Vue》', link: '/note/vue/' },
+              { text: '《React》', link: '/note/react/' }
+            ],
+          },
+        ],
       },
       {
         text: '索引',
